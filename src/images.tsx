b185f7fce34bdb8a22122e092bc29a8383f23495
Akanksha2596 <akanksha.yadav@brainvire.com>
2024-02-29 18:22:12 +0530

    images for slides

---
+++ src/images.tsx
@@ -0,0 +1,6 @@
+import img1 from "@/assests/images/1.jpg";
+import img2 from "@/assests/images/2.jpg";
+import img3 from "@/assests/images/3.jpg";
+import img4 from "@/assests/images/4.jpg";
+import img5 from "@/assests/images/5.jpg";
+export default [img1, img2, img3, img4, img5] ;
